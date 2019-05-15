c1ffc3db8db10204c31c60072400e9c21282c401
krook1024 <krook1024@gmail.com>
2019-05-15 21:13:56 +0200

    license everywhere 🗺🗺🗺🗺🗺

--- client/CalcSocket.py
+++ client/CalcSocket.py
@@ -1,3 +1,28 @@
+#!/usr/bin/env python3
+#            _            _
+#           | |          | |
+#   ___ __ _| | ___ _   _| |_   _ ___
+#  / __/ _` | |/ __| | | | | | | / __|    v0.1.0
+# | (_| (_| | | (__| |_| | | |_| \__ \
+#  \___\__,_|_|\___|\__,_|_|\__,_|___/
+#
+#
+#    Calculus - a simple terminal-based game based on socket programming
+#    Copyright (C) 2019 Molnár Antal Albert
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # This file stores the class CalcSocket for the project 'Calculus'.
 
 import socket
